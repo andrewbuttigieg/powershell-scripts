3355404181a6f4e997c7899d48f1b482dd7a5742
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2017-01-11 22:33:24 +0100

    Update kill-task-examples.ps1

--- kill-task-examples.ps1
+++ kill-task-examples.ps1
@@ -1,8 +1,8 @@
-#kill a process with PID 3512
+# Kills a process with PID 3512
 Stop-Process 3512
 
-#kill all processes matching the wild card
+# Kills all processes matching the wild card
 Stop-Process -processname ExecutableNameWithWilCard*
 
-#kill all processes on remote machine matching wild card
+# Kills all processes on remote machine matching wild card
 Invoke-Command -ComputerName ComputerName {Stop-Process Test*}
