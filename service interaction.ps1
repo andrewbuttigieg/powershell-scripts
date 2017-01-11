012429686007d2ec9323ccb1c350e80367bf0483
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2017-01-11 22:40:32 +0100

    Create service interaction.ps1

---
+++ service interaction.ps1
@@ -0,0 +1,8 @@
+# Starts a non started service
+Start-Service -Name "yourService*"
+
+# Stops a started service
+Stop-Service -Name "yourService*"
+
+# Restarts a started service
+Restart-Service -Name "yourService*"
